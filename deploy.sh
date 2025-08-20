4c91bee1895a4dbdac24c7afb1c56baea79e827d
lipelatorre <86213073+lipelatorre@users.noreply.github.com>
2025-08-19 21:40:46 -0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+kubectl apply -f k8s/namespace.yaml
+kubectl apply -f k8s/configmap.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+kubectl apply -f k8s/ingress.yaml
